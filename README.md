a574797a642582699531fd16c744ba5ca4e3a0a6
TariniHRamchandani <74418910+TariniHRamchandani@users.noreply.github.com>
2021-01-11 23:00:50 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# TheNewGametry1
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,28 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+var box1;
+
+function setup() {
+  createCanvas(800,400);
+  world = engine.world;
+
+    box1 = new Box(700,420,70,70);
+    box2 = new Box(700,520,70,70);
+    box3 = new Box(700,640,70,70);
+    box4 = new Box(700,740,70,70);
+    box5 = new Box(700,860,70,70);
+ // createSprite(400, 200, 50, 50);
+}
+
+function draw() {
+  background(255); 
+  box1.display();
+  box2.display();
+  box3.display();
+  box4.display();
+  box5.dispaly(); 
+  //drawSprites();
+}
